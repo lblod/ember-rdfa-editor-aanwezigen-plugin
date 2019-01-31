67e1a58faa6b4de2ee1a438242f371b086b0f205
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-01-31 17:54:18 +0100

    some apologetic comments from my part

--- addon/components/editor-plugins/overige-mandatarissen-aanwezigen-row.js
+++ addon/components/editor-plugins/overige-mandatarissen-aanwezigen-row.js
@@ -9,15 +9,19 @@ export default Component.extend({
     this._super(...arguments);
     this.set('aanwezig', this.aanwezige.aanwezig || false);
     this.set('mandataris', this.aanwezige.mandataris);
-    console.log(this.mandataris.id);
-    console.log(await this.mandataris.bekleedt);
+    //console.log(this.mandataris.id);
+    //console.log(await this.mandataris.bekleedt);
 
     // const test = await this.mandataris.get('bekleedt');
     // console.log(test);
 
     // this.set('functie', await this.mandataris.get('bekleedt').get('bestuursfunctie').get('label'));
     // console.log(this.functie);
+
+    //Comment felix: I am sooo sorry. The selecteer bestuursorgaan I hinted to use, did not contain any mandatarissen (for now)
+    //So best to select to select orgaan: burgemeester 2019 or gemeenteraad 2012-2019
     this.set('persoon', await this.mandataris.get('isBestuurlijkeAliasVan'));
+    console.log(this.persoon.achternaam);
   },
 
   actions: {
