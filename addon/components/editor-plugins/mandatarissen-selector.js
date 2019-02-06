2a5a9c5bd415b3086adc21214522ad43b2527f5c
Claire Lovisa <lovisa.claire@gmail.com>
2019-02-06 14:22:13 +0100

    Add small fixes

--- addon/components/editor-plugins/mandatarissen-selector.js
+++ addon/components/editor-plugins/mandatarissen-selector.js
@@ -2,10 +2,13 @@ import Component from '@ember/component';
 import layout from '../../templates/components/editor-plugins/mandatarissen-selector';
 import { inject as service } from '@ember/service';
 import { task, timeout } from 'ember-concurrency';
+import { reads } from '@ember/object/computed';
 
 export default Component.extend({
   layout,
   store: service(),
+  rdfaEditorAanwezigenPlugin: service(),
+  bestuursorgaanUri: reads('rdfaEditorAanwezigenPlugin.bestuursorgaanUri'),
 
   didReceiveAttrs(){
     this._super(...arguments);
@@ -16,6 +19,7 @@ export default Component.extend({
     yield timeout(300);
     let queryParams = {
       include: 'is-bestuurlijke-alias-van,bekleedt,bekleedt.bestuursfunctie',
+      'filter[bekleedt][bevat-in][:uri:]': this.bestuursorgaanUri,
       'filter[is-bestuurlijke-alias-van][achternaam]': searchData,
       page: { size: 100 },
       sort: 'is-bestuurlijke-alias-van.achternaam'
--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 'use strict';
 
 module.exports = {
-  isDevelopingAddon() {
-    return true;
-  },
   name: require('./package').name
 };
