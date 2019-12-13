4807cfee87a023c28e0c1b3a0dccecd8506f01a3
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2019-12-13 16:26:53 +0100

    update voorzitter to mandataris

--- addon/components/editor-plugins/mandatarissen-selector.js
+++ addon/components/editor-plugins/mandatarissen-selector.js
@@ -10,28 +10,19 @@ export default Component.extend({
   rdfaEditorAanwezigenPlugin: service(),
   bestuursorgaanUri: reads('rdfaEditorAanwezigenPlugin.bestuursorgaanUri'),
 
-  didReceiveAttrs(){
-    this._super(...arguments);
-    this.set('_mandataris', this.mandataris);
-  },
-
   searchByName: task(function* (searchData) {
     yield timeout(300);
     let queryParams = {
-      include: 'is-bestuurlijke-alias-van,is-bestuurlijke-alias-van.geboorte,bekleedt,bekleedt.bestuursfunctie',
       'filter[bekleedt][bevat-in][:uri:]': this.bestuursorgaanUri,
       'filter[is-bestuurlijke-alias-van][achternaam]': searchData,
       page: { size: 100 },
       sort: 'is-bestuurlijke-alias-van.achternaam'
     };
-
-    let mandatarissen = yield this.store.query('mandataris', queryParams);
-    return mandatarissen;
+    return yield this.store.query('mandataris', queryParams);
   }),
 
   actions: {
     async select(mandataris){
-      this.set('_mandataris', mandataris);
       this.onSelect(mandataris);
     }
 }
