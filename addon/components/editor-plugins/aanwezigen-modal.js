699059eca9cc62c501e9f3161fab6a3b9e6547c4
Oscar <lagartoverde97@gmail.com>
2020-04-01 17:39:36 +0200

    converted aanwezigen modal to class

--- addon/components/editor-plugins/aanwezigen-modal.js
+++ addon/components/editor-plugins/aanwezigen-modal.js
@@ -1,36 +1,44 @@
 import Component from '@ember/component';
 import layout from '../../templates/components/editor-plugins/aanwezigen-modal';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
-export default Component.extend({
-  layout,
-  verkozenGevolgUri: 'http://data.vlaanderen.be/id/concept/VerkiezingsresultaatGevolgCode/89498d89-6c68-4273-9609-b9c097727a0f',
-  store: service(),
-  viewMode: 'default',
-
-  actions: {
-    selectVoorzitter(mandataris){
-      this.set('voorzitter', mandataris);
-    },
-
-    selectSecretaris(functionaris){
-      this.set('secretaris', functionaris);
-    },
-
-    createPerson(){
-      this.set('viewMode', 'createPerson');
-    },
-
-    goHome(){
-      this.set('viewMode', 'default');
-    },
-
-    cancelCreatePerson(){
-      this.set('viewMode', 'default');
-    },
-
-    finishCreatePerson(){
-      this.set('viewMode', 'default');
-    }
+export default class AanwezigenModal extends Component {
+  @service store;
+  constructor() {
+    super(...arguments)
+    this.layout = layout;
+    this.verkozenGevolgUri = 'http://data.vlaanderen.be/id/concept/VerkiezingsresultaatGevolgCode/89498d89-6c68-4273-9609-b9c097727a0f';
+    this.viewMode = 'default'
   }
-});
+
+  @action
+  selectVoorzitter(mandataris){
+    this.set('voorzitter', mandataris);
+  }
+
+  @action
+  selectSecretaris(functionaris){
+    this.set('secretaris', functionaris);
+  }
+
+  @action
+  createPerson(){
+    this.set('viewMode', 'createPerson');
+  }
+
+  @action
+  goHome(){
+    this.set('viewMode', 'default');
+  }
+
+  @action
+  cancelCreatePerson(){
+    this.set('viewMode', 'default');
+  }
+
+  @action
+  finishCreatePerson(){
+    this.set('viewMode', 'default');
+  }
+}
